9d93dea600be43f50f570b4fa1d33c2d99d7f921
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2022-12-19 21:52:59 -0600

    search input added

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -14,13 +14,17 @@ constructor(){
 
 componentDidMount(){
   fetch('https://jsonplaceholder.typicode.com/users')
-    .then((responce) => console.log(responce)
+    .then((responce) => responce.json()
+    .then((users) => this.setState(() => {
+      return {monsters: users}
+    },))
     );
 }
 
   render(){
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='Search Monsters' onChange={(event) => {}}/>
         {
           this.state.monsters.map((monster) => {
             return<div key = {monster.id}><h1>{monster.name}</h1></div>
